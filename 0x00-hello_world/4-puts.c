12e29e72304b88900217420625fd22734f0e50a0
sargon-kiki <kikinem2016@gmail.com>
2023-02-16 13:05:15 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A c program that prints a line with the put function
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return  (0);
+}
+
