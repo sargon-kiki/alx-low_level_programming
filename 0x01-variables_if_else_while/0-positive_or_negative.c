66467301d0af32bc5e07a508569c3c6a04b7c9e5
sargon-kiki <kikinem2016@gmail.com>
2023-02-17 15:47:16 -0800

    0 fixed

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,10 +5,11 @@
  *  * main - Entry point
  *   *
  *    * Return: Always 0 (Success)
- *     */
+*/
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
